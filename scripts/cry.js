7844927047f4652c345fd1abcbf3fa2dd1739d94
JWJ212 <jackwjamison@gmail.com>
2024-06-12 22:48:57 -0500

    fix audio maybe?

--- scripts/cry.js
+++ scripts/cry.js
@@ -3,7 +3,7 @@ function playclip() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
  {
-  document.all.sound.src = "https://cdn.regirock.net/audio/cry.mp3";
+  document.all.sound.src = "/cdn/audio/cry.mp3";
  }
 }
 
